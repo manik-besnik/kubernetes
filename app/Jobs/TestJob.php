d38798587c59a59d6d5a2071c8e632a6b97ccb8a
Manik Mia <135939845+manik-besnik@users.noreply.github.com>
2025-03-22 16:23:09 +0600

    update

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class TestJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        $this->queue = 'test';
+
+        $this->attempts();
+
+
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+
+    }
+
+}
